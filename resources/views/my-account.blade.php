9c8622bc4e8426f1ba0b710e5a24b504f4eadeb6
EL MEHDI AIT FAKIR <elmehdiaitfakir@gmail.com>
2022-11-22 23:47:31 +0100

    clean menu of my-account page

--- resources/views/my-account.blade.php
+++ resources/views/my-account.blade.php
@@ -26,26 +26,17 @@
                     <!-- Nav tabs -->
                     <div class="dashboard_tab_button" data-aos="fade-up" data-aos-delay="0">
                         <ul role="tablist" class="nav flex-column dashboard-list">
-                            <li><a href="#dashboard" data-bs-toggle="tab" class="nav-link active">Dashboard</a></li>
-                            <li> <a href="#orders" data-bs-toggle="tab" class="nav-link">Orders</a></li>
+                            <li> <a href="#orders" data-bs-toggle="tab" class="nav-link active">Orders</a></li>
                             <li><a href="#downloads" data-bs-toggle="tab" class="nav-link">Downloads</a></li>
-                            <li><a href="#address" data-bs-toggle="tab" class="nav-link">Addresses</a></li>
                             <li><a href="#account-details" data-bs-toggle="tab" class="nav-link">Account details</a>
                             </li>
-                            <li><a href="login.html" class="nav-link">logout</a></li>
                         </ul>
                     </div>
                 </div>
                 <div class="col-sm-12 col-md-9 col-lg-9">
                     <!-- Tab panes -->
                     <div class="tab-content dashboard_content" data-aos="fade-up" data-aos-delay="200">
-                        <div class="tab-pane fade show active" id="dashboard">
-                            <h4>Dashboard </h4>
-                            <p>From your account dashboard. you can easily check &amp; view your <a href="#">recent
-                                    orders</a>, manage your <a href="#">shipping and billing addresses</a> and <a
-                                    href="#">Edit your password and account details.</a></p>
-                        </div>
-                        <div class="tab-pane fade" id="orders">
+                        <div class="tab-pane fade show active" id="orders">
                             <h4>Orders</h4>
                             <div class="table_page table-responsive">
                                 <table>
@@ -105,21 +96,6 @@
                                     </tbody>
                                 </table>
                             </div>
-                        </div>
-                        <div class="tab-pane" id="address">
-                            <p>The following addresses will be used on the checkout page by default.</p>
-                            <h5 class="billing-address">Billing address</h5>
-                            <a href="#" class="view">Edit</a>
-                            <p class="mb-2"><strong>Michael M Hoskins</strong></p>
-                            <address>
-                                <span class="mb-1 d-inline-block"><strong>City:</strong> Seattle</span>,
-                                <br>
-                                <span class="mb-1 d-inline-block"><strong>State:</strong> Washington(WA)</span>,
-                                <br>
-                                <span class="mb-1 d-inline-block"><strong>ZIP:</strong> 98101</span>,
-                                <br>
-                                <span><strong>Country:</strong> USA</span>
-                            </address>
                         </div>
                         <div class="tab-pane fade" id="account-details">
                             <h3>Account details </h3>
