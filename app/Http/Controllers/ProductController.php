4b50336637cbd1d81a5b826330ef70ceb86e4615
EL MEHDI AIT FAKIR <elmehdiaitfakir@gmail.com>
2022-12-09 15:52:10 +0100

    starting on articles menu

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -68,7 +68,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        // return view('products.index');
+        return view('admin.articles.index');
     }
 
     /**
